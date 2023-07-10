f9a65f80aa36e2437137cdeb205bfe0daf822d8f
mandalaad <mandalacs18@gmail.com>
2023-07-11 03:13:27 +0700

    third commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './HeroStyle.css'
 import robotimg1 from '../../assets/20220127_043450.jpg'
+import fotogue from '../../assets/IMG_20220225_215718_191.jpg'
 import { Link } from 'react-router-dom'
 function Hero() {
+    const [isFullSize, setIsFullSize] = useState(false);
+
+    const handleClick = () => {
+      setIsFullSize(!isFullSize);
+    };
   return (
     <div className='hero'>
         <div className='mask'>
             <img className='into-img' src={robotimg1} alt="robotimg1" />
         </div>
         <div className='content'>
+            
+                <img className={`foto ${isFullSize ? 'full-size' : ''}`} src={fotogue} alt="" onClick={handleClick}/>
+            
+            <h3>Tri Mandala Adi Dalem</h3>
             <p>HI, I'M A FRONTEND DEVELOPER</p>
             <h1>Frontend Developer</h1>
             <div>
